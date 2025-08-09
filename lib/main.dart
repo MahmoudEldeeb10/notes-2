8af6a649030b90dbbbb1ebec130ee99772e14be2
MahmoudEldeeb10 <nett445g@gmail.com>
2025-08-09 22:25:58 +0300

    create app bar

--- lib/main.dart
+++ lib/main.dart
@@ -11,17 +11,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        // appBar: AppBar(
-        //   title: const Text('notes'),
-        //   actions: [
-        //     IconButton(
-        //       icon: const Icon(Icons.search),
-        //       onPressed: () {
-        //         // Action to perform when the button is pressed
-        //       },
-        //     ),
-        //   ],
-        // ),
+        appBar: AppBar(
+          title: const Text('notes'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () {
+                // Action to perform when the button is pressed
+              },
+            ),
+          ],
+        ),
         // body: const Center(child: Text('Hello, world!')),
       ),
     );
